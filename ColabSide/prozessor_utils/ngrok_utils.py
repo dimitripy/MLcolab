314a389a34da50f5e03d3319192799a8828d0a5b
dimitripy <waldschmidt92@googlemail.com>
2024-09-21 15:05:50 +0000

    Port und pfade in .env verlagert und besseres Fehlerhandling

--- ColabSide/prozessor_utils/ngrok_utils.py
+++ ColabSide/prozessor_utils/ngrok_utils.py
@@ -3,19 +3,25 @@ import asyncio
 from pyngrok import ngrok
 
 async def start_ngrok(authtoken, port):
-    ngrok.kill()  # Beendet ngrok, falls es bereits läuft
-    await run_process(['ngrok', 'config', 'add-authtoken', authtoken])
-    await run_process(['ngrok', 'http', '--log', 'stderr', str(port), '--host-header', f'localhost:{port}'])
+    try:
+        ngrok.kill()  # Beendet ngrok, falls es bereits läuft
+        await run_process(['ngrok', 'config', 'add-authtoken', authtoken])
+        await run_process(['ngrok', 'http', '--log', 'stderr', str(port), '--host-header', f'localhost:{port}'])
+    except Exception as e:
+        print(f"Fehler beim Starten von ngrok: {str(e)}")
 
 async def run_process(cmd):
-    p = await asyncio.subprocess.create_subprocess_exec(
-        *cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
+    try:
+        p = await asyncio.create_subprocess_exec(
+            *cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
 
-    async def pipe(lines):
-        async for line in lines:
-            print(line.strip().decode('utf-8'))
+        async def pipe(lines):
+            async for line in lines:
+                print(line.strip().decode('utf-8'))
 
-    await asyncio.gather(pipe(p.stdout), pipe(p.stderr))
+        await asyncio.gather(pipe(p.stdout), pipe(p.stderr))
+    except Exception as e:
+        print(f"Fehler beim Ausführen des Prozesses: {str(e)}")
